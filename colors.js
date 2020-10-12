df8f9e5a4925da236d55825b78f86e9cb166bd42
Baru Yogesh <bhargav.yogesh@gmail.com>
2020-10-11 21:49:38 -0400

    changed template

---
+++ colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  background: '#474747',
+  backgroundDark: '#f0e6f6',
+
+  text: '#85B404',
+
+  primary: '#2D7DD2',
+  primaryLight: '#85B404',
+  primaryDark: '#2D7DD2',
+
+  secondary: '#85B404',
+  secondaryLight: '#85B404',
+  secondaryDark: '#2D7DD2',
+};
